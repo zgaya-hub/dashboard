2908c60c40f74f09009d6fc63e890ce78ba746f4
Zohaib Shahid <z-shaid@ejada.com>
2024-01-16 21:48:17 +0500

    feat: google auth

--- src/feature/SignUp/screens/SignUpInputScreen.tsx
+++ src/feature/SignUp/screens/SignUpInputScreen.tsx
@@ -1,18 +1,10 @@
 import Page from "@/components/Page";
 import SignUpForm from "../components/SignUpForm";
-import { SignUpFormDataInterface } from "../types";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import useFirebase from "@/context/FirebaseContext";
 
 export default function SignUpInputScreen() {
-  const { auth } = useFirebase();
-  const handleOnCallSignIn = async (formData: SignUpFormDataInterface) => {
-    const result = await createUserWithEmailAndPassword(auth, formData.email, formData.password)
-    console.log(result);
-  };
   return (
     <Page>
-      <SignUpForm onSubmit={handleOnCallSignIn} />
+      <SignUpForm />
     </Page>
   );
 }
